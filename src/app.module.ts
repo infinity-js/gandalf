996b5cb30ea5e31b26bc2723a2214a09d3c00dce
WhiteAngel13 <edmilsonsw22@gmail.com>
2023-09-18 15:03:57 -0400

    feat: implements config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { gandalfConfigFile } from './config/config-file.validator';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [gandalfConfigFile],
+      expandVariables: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/config-file.validator.ts
@@ -0,0 +1,43 @@
+import { readFile } from 'fs/promises';
+import { load } from 'js-yaml';
+import { join } from 'path';
+import { z } from 'zod';
+
+const filePath = join(__dirname, '../../gandalf.config.yml');
+
+const gandalfConfigSchema = z.object({
+  integrations: z.object({
+    google_oauth: z
+      .object({
+        client_id: z.string(),
+        client_secret: z.string(),
+      })
+      .optional(),
+  }),
+});
+
+export type GandalfConfigSchema = z.infer<typeof gandalfConfigSchema>;
+
+export const parseEnvVars = (yml: string) => {
+  //Find env using regex ${ENV_VAR}
+  const envRegex = /\${(.*?)}/g;
+  const envVars = yml.match(envRegex) || [];
+
+  envVars.forEach((envVar) => {
+    const envVarName = envVar.replace('${', '').replace('}', '');
+    const envVarValue = process.env[envVarName];
+    yml = yml.replaceAll(envVar, envVarValue || '');
+  });
+
+  return yml;
+};
+
+export const gandalfConfigFile = async () => {
+  const ymlFile = await readFile(filePath, 'utf8');
+  const ymlWithEnv = parseEnvVars(ymlFile);
+  const ymlParsed = load(ymlWithEnv);
+
+  const ymlValidated = gandalfConfigSchema.parse(ymlParsed);
+
+  return ymlValidated;
+};
